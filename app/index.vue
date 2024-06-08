fde9da20873eba9c0d7b0476e84e19c97f136b6b
Dmitriy Karpov <dmtkpv@gmail.com>
2024-06-08 19:09:10 +0300

    app: base design

--- app/index.vue
+++ app/index.vue
@@ -4,6 +4,36 @@
 
 <style lang="scss">
 
+    html {
+        height: 100%;
+    }
+
+    body {
+        height: 100%;
+        font-family: Arial, sans-serif;
+        line-height: 1;
+    }
+
+    #app {
+        height: 100%;
+        display: flex;
+        flex-flow: column;
+        align-items: center;
+        justify-content: center;
+
+        h1 {
+            font-size: 64px;
+            margin: 0;
+        }
+
+        button {
+            width: 120px;
+            height: 80px;
+            margin: 48px 0 112px 0;
+        }
+
+    }
+
 </style>
 
 
@@ -12,8 +42,10 @@
 -->
 
 <template>
-    <div>{{ count }}</div>
-    <button @click="count++">Click</button>
+    <div id="app">
+        <h1>{{ count }}</h1>
+        <button @click="count++">Click</button>
+    </div>
 </template>
 
 
